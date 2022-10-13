6ed2f29975344d3402b34ec320e735a320bd669b
QuinMeow <nicopoiduang5228@gmail.com>
2022-10-13 16:22:20 +0800

    实现图像平移与缩放

--- Main.py
+++ Main.py
@@ -14,6 +14,7 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(MyMainWindow, self).__init__(parent)
         self.setupUi(self)
+        self.MainView = MyMainView(self.MainView)
         # 初始化成员对象
         self.im_width = 0
         self.im_height = 0
@@ -27,10 +28,17 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
         self.Btn_ChooseFile.clicked.connect(self.openfile)
 
     def openfile(self):
+        """
+        选择要打开的文件路径
+        """
         openfile_name = QFileDialog.getOpenFileName(self, '选择文件', './', 'All Files(*)')
         self.LinEdit_ImageAddress.setText(openfile_name[0])
 
     def read_img(self):
+        """
+        读取影像，如非8位无符号进行转换
+        :return:
+        """
         filename = self.LinEdit_ImageAddress.text()
         dataset = gdal.Open(filename)  # 打开文件
         self.im_width = dataset.RasterXSize  # 栅格矩阵的列数
@@ -69,18 +77,146 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
         return int_data
 
     def show_img(self):
+        """
+        显示影像到MainView
+        :return:
+        """
         Band = np.array((int(self.SpBox_Red.value()) - 1, int(self.SpBox_Green.value()) - 1, \
                          int(self.SpBox_Blue.value()) - 1))
         show_img = self.qim_data[:, :, Band]
         height, width, bytesPerComponent = show_img.shape
-        size = QSize(self.MainView.size().width() - 10, self.MainView.size().height() - 10)
+        # size = QSize(self.MainView.size().width() - 10, self.MainView.size().height() - 10)
         bytesPerLine = bytesPerComponent * width
         self.QImg = QImage(show_img.tobytes(), width, height, bytesPerLine, QImage.Format_RGB888)
-        self.pixmap = QPixmap.fromImage(self.QImg).scaled(size, Qt.KeepAspectRatio, Qt.SmoothTransformation)
-        self.item = QGraphicsPixmapItem(self.pixmap)
-        self.scene = QGraphicsScene()  # 创建场景
-        self.scene.addItem(self.item)
-        self.MainView.setScene(self.scene)
+        # self.pixmap = QPixmap.fromImage(self.QImg).scaled(size, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+        # self.item = QGraphicsPixmapItem(self.pixmap)
+        # self.scene = QGraphicsScene()  # 创建场景
+        # self.scene.addItem(self.item)
+        # self.MainView.setScene(self.scene)
+        self.MainView.addLayer(self.QImg)
+
+
+class MyMainView(QGraphicsView):
+    def __init__(self, graphicsView):
+        super().__init__()
+        self.graphicsView = graphicsView
+
+        self.graphicsView.setStyleSheet("padding: 0px; border: 0px;")  # 内边距和边界去除
+        self.scene = QtWidgets.QGraphicsScene(self)
+        self.graphicsView.setAlignment(Qt.AlignLeft | Qt.AlignTop)  # 改变对齐方式
+
+        self.graphicsView.setSceneRect(0, 0, self.graphicsView.viewport().width(),
+                                       self.graphicsView.height())  # 设置图形场景大小和图形视图大小一致
+        self.graphicsView.setScene(self.scene)
+
+        self.scene.mousePressEvent = self.scene_MousePressEvent  # 接管图形场景的鼠标点击事件
+        # self.scene.mouseReleaseEvent = self.scene_mouseReleaseEvent
+        self.scene.mouseMoveEvent = self.scene_mouseMoveEvent  # 接管图形场景的鼠标移动事件
+        self.scene.wheelEvent = self.scene_wheelEvent  # 接管图形场景的滑轮事件
+
+        # self.size = QSize(self.size().width() - 10, self.size().height() - 10) #初始大小
+        self.ratio = 1  # 缩放初始比例
+        self.zoom_step = 0.1  # 缩放步长
+        self.zoom_max = 2  # 缩放最大值
+        self.zoom_min = 0.2  # 缩放最小值
+        self.pixmapItem = None
+
+    def addLayer(self, image):
+        """
+        将外部的QImage放入scene
+        :param item:
+        :return:
+        """
+        if self.pixmapItem != None:
+            originX = self.pixmapItem.x()
+            originY = self.pixmapItem.y()
+        else:
+            originX, originY = 0, 0
+
+        self.pixmap = QPixmap.fromImage(image)
+        self.pixmapItem = self.scene.addPixmap(self.pixmap)
+        self.pixmapItem.setScale(self.ratio)
+        self.pixmapItem.setPos(originX, originY)
+
+    def scene_MousePressEvent(self, event):
+        if event.button() == Qt.MidButton:  # 中键按下
+            self.preMousePosition = event.scenePos()  # 获取鼠标当前位置
+
+    def scene_mouseMoveEvent(self, event):
+        if event.buttons() == Qt.MidButton:
+            self.MouseMove = event.scenePos() - self.preMousePosition  # 鼠标当前位置-先前位置=单次偏移量
+            self.preMousePosition = event.scenePos()  # 更新当前鼠标在窗口上的位置，下次移动用
+            self.pixmapItem.setPos(self.pixmapItem.pos() + self.MouseMove)  # 更新图元位置
+
+    def scene_wheelEvent(self, event):
+        angle = event.delta() / 8  # 返回QPoint对象，为滚轮转过的数值，单位为1/8度
+        if angle > 0:
+            # print("滚轮上滚")
+            self.ratio += self.zoom_step  # 缩放比例自加
+            if self.ratio > self.zoom_max:
+                self.ratio = self.zoom_max
+            else:
+                w = self.pixmap.size().width() * (self.ratio - self.zoom_step)
+                h = self.pixmap.size().height() * (self.ratio - self.zoom_step)
+                x1 = self.pixmapItem.pos().x()  # 图元左位置
+                x2 = self.pixmapItem.pos().x() + w  # 图元右位置
+                y1 = self.pixmapItem.pos().y()  # 图元上位置
+                y2 = self.pixmapItem.pos().y() + h  # 图元下位置
+                if event.scenePos().x() > x1 and event.scenePos().x() < x2 \
+                        and event.scenePos().y() > y1 and event.scenePos().y() < y2:  # 判断鼠标悬停位置是否在图元中
+                    # print('在内部')
+                    self.pixmapItem.setScale(self.ratio)  # 缩放
+                    a1 = event.scenePos() - self.pixmapItem.pos()  # 鼠标与图元左上角的差值
+                    a2 = self.ratio / (self.ratio - self.zoom_step) - 1  # 对应比例
+                    delta = a1 * a2
+                    self.pixmapItem.setPos(self.pixmapItem.pos() - delta)
+                    # ----------------------------分维度计算偏移量-----------------------------
+                    # delta_x = a1.x()*a2
+                    # delta_y = a1.y()*a2
+                    # self.pixmapItem.setPos(self.pixmapItem.pos().x() - delta_x,
+                    #                        self.pixmapItem.pos().y() - delta_y)  # 图元偏移
+                    # -------------------------------------------------------------------------
+
+                else:
+                    # print('在外部')  # 以图元中心缩放
+                    self.pixmapItem.setScale(self.ratio)  # 缩放
+                    delta_x = (self.pixmap.size().width() * self.zoom_step) / 2  # 图元偏移量
+                    delta_y = (self.pixmap.size().height() * self.zoom_step) / 2
+                    self.pixmapItem.setPos(self.pixmapItem.pos().x() - delta_x,
+                                           self.pixmapItem.pos().y() - delta_y)  # 图元偏移
+        else:
+            # print("滚轮下滚")
+            self.ratio -= self.zoom_step
+            if self.ratio < 0.2:
+                self.ratio = 0.2
+            else:
+                w = self.pixmap.size().width() * (self.ratio + self.zoom_step)
+                h = self.pixmap.size().height() * (self.ratio + self.zoom_step)
+                x1 = self.pixmapItem.pos().x()
+                x2 = self.pixmapItem.pos().x() + w
+                y1 = self.pixmapItem.pos().y()
+                y2 = self.pixmapItem.pos().y() + h
+                # print(x1, x2, y1, y2)
+                if event.scenePos().x() > x1 and event.scenePos().x() < x2 \
+                        and event.scenePos().y() > y1 and event.scenePos().y() < y2:
+                    # print('在内部')
+                    self.pixmapItem.setScale(self.ratio)  # 缩放
+                    a1 = event.scenePos() - self.pixmapItem.pos()  # 鼠标与图元左上角的差值
+                    a2 = self.ratio / (self.ratio + self.zoom_step) - 1  # 对应比例
+                    delta = a1 * a2
+                    self.pixmapItem.setPos(self.pixmapItem.pos() - delta)
+                    # ----------------------------分维度计算偏移量-----------------------------
+                    # delta_x = a1.x()*a2
+                    # delta_y = a1.y()*a2
+                    # self.pixmapItem.setPos(self.pixmapItem.pos().x() - delta_x,
+                    #                        self.pixmapItem.pos().y() - delta_y)  # 图元偏移
+                    # -------------------------------------------------------------------------
+                else:
+                    # print('在外部')
+                    self.pixmapItem.setScale(self.ratio)
+                    delta_x = (self.pixmap.size().width() * self.zoom_step) / 2
+                    delta_y = (self.pixmap.size().height() * self.zoom_step) / 2
+                    self.pixmapItem.setPos(self.pixmapItem.pos().x() + delta_x, self.pixmapItem.pos().y() + delta_y)
 
 
 # 程序主入口
